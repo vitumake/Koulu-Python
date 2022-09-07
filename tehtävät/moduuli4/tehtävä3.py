9919d25fa11143c934bccfe276354dbba8100f0e
markus <mag3768@gmail.com>
2022-09-07 13:00:22 +0300

    parannus

--- tehtävät/moduuli4/tehtävä3.py
+++ tehtävät/moduuli4/tehtävä3.py
@@ -3,6 +3,6 @@ while ((luku.strip('-')).replace('.', '')).isnumeric():
     if float(luku)<float(pieni):
         pieni=float(luku)
     elif float(luku)>float(suuri):
-        suuri=float(luku)
+        suuri=float(luku)-1
     luku=input('Anna luku: ')
 print('Suurin luku: ' + str(suuri) + '\nPienin luku: ' + str(pieni))
--- tehtävät/moduuli4/tehtävä6.py
+++ tehtävät/moduuli4/tehtävä6.py
@@ -1,9 +1,9 @@
-import math
 import random
+import time
 n=0
 N=int(input('Anna testattavien pistiden määrä: '))
+ts=time.time()
 for i in range(N):
     if pow(random.uniform(-1, 1), 2) + pow(random.uniform(-1, 1), 2) < 1:
         n+=1
-print('Pi:n likiarvo on: ' + str(4*n/N))
-print('Pi:n vakio math librarysta: ' + str(math.pi))
+print(f"Pi:n likiarvo on: {4*n/N} Ajassa {round(time.time() - ts, 3)}s")
