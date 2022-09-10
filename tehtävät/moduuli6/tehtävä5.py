fb3a21626b7b009911f89087431dca1380f23291
markus <mag3768@gmail.com>
2022-09-10 10:39:47 +0300

    parannus

--- tehtävät/moduuli6/tehtävä5.py
+++ tehtävät/moduuli6/tehtävä5.py
@@ -1,8 +1,9 @@
 import random
 def delOddN(arr):
+    evenArr=[]
     for i in arr:
-        if i%2!=0: arr.remove(i)
-    return arr
+        if i%2==0: evenArr.append(i)
+    return evenArr
 testArr=[]
 for i in range(10):
     testArr.append(random.randint(0, 100))
